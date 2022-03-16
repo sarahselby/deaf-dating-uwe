1231221185b157b0b09cec2c631e7096c396c71f
Sarah Cook <Sarah.Cook@uwe.ac.uk>
2022-03-16 16:14:24 +0000

    added files

---
+++ script.js
@@ -0,0 +1,15 @@
+function playMusic () {
+	var audio = new Audio("assets/audio/guitar_mixdown.wav")
+	audio.play();
+}
+function playCooking () {
+	var audio = new Audio("assets/audio/cooking.wav")
+	audio.play();
+}
+
+function playDance () {
+	var audio = new Audio("assets/audio/pac-man.wav")
+	audio.play();
+}
+
+
